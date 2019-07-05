504253003be58668f4365884a753b496a04ccf57
Aashish Tamsya <aashish.tamsya@gmail.com>
2019-07-05 10:31:48 +0530

    Initial Commit

---
+++ SearchBar/SearchBar.h
@@ -0,0 +1,19 @@
+//
+//  SearchBar.h
+//  SearchBar
+//
+//  Created by Aashish Tamsya on 12/07/19.
+//  Copyright © 2019 Aashish Tamsya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SearchBar.
+FOUNDATION_EXPORT double SearchBarVersionNumber;
+
+//! Project version string for SearchBar.
+FOUNDATION_EXPORT const unsigned char SearchBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SearchBar/PublicHeader.h>
+
+
